594e1cc74748d674f634ebd1a3017c8aee3459e2
Mukkupretski <mukkupretski@gmail.com>
2024-03-29 13:25:13 +0200

    Added results and specific info

--- drawapp.py
+++ drawapp.py
@@ -1,5 +1,6 @@
 import pygame
 from main import guess
+import math
 import numpy as np
 from scipy.signal import convolve2d
 
@@ -22,6 +23,10 @@ class Button():
         x -= self.rect.x
         y -= self.rect.y
         return (0<x<BUTTON_WIDTH and 0<y<BUTTON_HEIGHT)
+    def setText(self,txt):
+        self.text = txt
+
+button = None
 
 ORANGE = (255,165,0)
 WHITE = (255,255,255)
@@ -33,7 +38,10 @@ HEIGHT = 400
 BUTTON_HEIGHT = 20
 BUTTON_WIDTH = 80
 BUTTON_GAP = 20
+BUTTON_HOP = 16
 
+RESULT_PADDING_TOP = 60
+RESULT_GAP = 10
 
 PIXEL_SIZE = 10
 
@@ -41,11 +49,14 @@ PADDING_LEFT = 40
 PADDING_TOP = 40
 
 COMICSANS = pygame.font.SysFont("comicsans", 16)
+COMICSANS2 = pygame.font.SysFont("comicsans", 10)
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Piirtoalusta")
 
 state = np.zeros(784)
+results = np.zeros(10)
+showresults = False
 
 def draw_canvas():
     global state
@@ -55,6 +66,12 @@ def draw_canvas():
         WIN.fill(color, (i%28*PIXEL_SIZE+PADDING_LEFT, i//28*PIXEL_SIZE+PADDING_TOP, PIXEL_SIZE, PIXEL_SIZE))
     for button in Button.buttons:
         button.draw()
+    result = COMICSANS.render(f"Arvaus: {np.argmax(results)} ({round(results.max()*100,1)}%)",1, BLACK)
+    WIN.blit(result, (PADDING_LEFT + 28*PIXEL_SIZE+(WIDTH-(PADDING_LEFT + 28*PIXEL_SIZE))/2-result.get_width()/2, RESULT_PADDING_TOP))
+    if showresults:
+        for i in range(10):
+            txt = COMICSANS2.render(f"{i}: {round(results[i]*100,1)}%",1, BLACK)
+            WIN.blit(txt, (PADDING_LEFT + 28*PIXEL_SIZE+(WIDTH-(PADDING_LEFT + 28*PIXEL_SIZE))/2-txt.get_width()/2,RESULT_PADDING_TOP+txt.get_height()*(i+1)+BUTTON_HEIGHT+RESULT_GAP*(i+2)))
     pygame.display.update()
 
 
@@ -66,20 +83,28 @@ def clear():
     set_state(np.zeros(784))
 
 def recognize():
-    guess(state)
+    global results
+    results = guess(state)
 
-def convolve():
+def blur():
     global state
-
     state = convolve2d(state.reshape((28,28)), np.ones((3,3))/9, mode="same").reshape((784))
-
+def toggleShow():
+    global showresults
+    showresults = not showresults
+    txt = "Näytä lisää"
+    if showresults:
+        txt = "Piilota"
+    button.setText(txt)
 
 def main():
+    global button
     run = True
     mousedown = False
     Button(PADDING_LEFT + 14*PIXEL_SIZE-BUTTON_GAP-BUTTON_WIDTH*3/2, PADDING_TOP+28*PIXEL_SIZE+(HEIGHT-(PADDING_TOP+28*PIXEL_SIZE))/2-BUTTON_HEIGHT, "Tyhjennä", clear)
-    Button(PADDING_LEFT + 14*PIXEL_SIZE-BUTTON_WIDTH/2, PADDING_TOP+28*PIXEL_SIZE+(HEIGHT-(PADDING_TOP+28*PIXEL_SIZE))/2-BUTTON_HEIGHT, "Sumenna", convolve)
+    Button(PADDING_LEFT + 14*PIXEL_SIZE-BUTTON_WIDTH/2, PADDING_TOP+28*PIXEL_SIZE+(HEIGHT-(PADDING_TOP+28*PIXEL_SIZE))/2-BUTTON_HEIGHT, "Sumenna", blur)
     Button(PADDING_LEFT + 14*PIXEL_SIZE+BUTTON_WIDTH/2+BUTTON_GAP, PADDING_TOP+28*PIXEL_SIZE+(HEIGHT-(PADDING_TOP+28*PIXEL_SIZE))/2-BUTTON_HEIGHT, "Tunnista", recognize)
+    button = Button(PADDING_LEFT + 28*PIXEL_SIZE+(WIDTH-(PADDING_LEFT + 28*PIXEL_SIZE))/2-BUTTON_WIDTH/2, RESULT_PADDING_TOP+BUTTON_HOP+RESULT_GAP, "Näytä lisää", toggleShow)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
